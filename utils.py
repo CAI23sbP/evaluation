807664b5a348a5fa0d8947ea7f45b1803c91d190
Sang baek, Park <108871750+CAI23sbP@users.noreply.github.com>
2023-10-25 19:06:28 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,13 @@ class Utils:
             return []
         if d =="[]":
             return []
-        return np.array(d.replace("[", "").replace("]", "").replace("array(","").replace(")","").split(r", ")).astype(float)
+        original_list = d.replace("[", "").replace("]", "").replace("array(","").replace(")","").split(r", ")
+        original_list = [item.replace('\n', '').split(',') for item in original_list]
+        cleaned_list = []
+        for sublist in original_list:
+            sub_cleaned = [x.strip().split(',') for x in sublist if x.strip()]
+            cleaned_list.extend(sub_cleaned)
+        return np.array(cleaned_list).astype(float)
     
     @staticmethod
     def speical_trans2(d):
